6312b57d73de16a0daa746c67255e508e92672b3
Franco Mutz <fmutz@itba.edu.ar>
2024-10-24 12:25:59 -0300

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,53 +3,65 @@ from central import Central
 import re
 
 class MailApp(Aplicacion): #Pertenece a cada telefono
-    cuentas = [] #Para chequear si el mail ya existe
-    
     def __init__(self, numero, central:Central):
         super().__init__("Mail", 100, True)
         self.numero = numero
         self.central = central
-        self.cuenta = None
+        self.cuenta_mail = None
         self.cuenta_iniciada = False
         self.mensajes = ListaMail()
     
     def enviar_mail(self, receptor, mensaje):
         #Le consulta a la central si el emisor tiene LTE
-        pass
+        if self.central.consultar_LTE(self.numero):
+            if self.cuenta_iniciada:
+                #Esto hay q verlo bien
+                self.mensajes.agregar_mail(mensaje)
+                print(f"Mensaje enviado a {receptor} con éxito")
+            else:
+                raise ValueError("No se pudo enviar el mensaje. Inicie sesión para continuar")
+        else:
+            raise ValueError("No se pudo enviar el mensaje. No tiene cobertura LTE")
     
-    def iniciar_sesion(self, cuenta, contrasenia):
-        pass
+    def iniciar_sesion(self, mail, contrasenia):
+        if mail in CuentaMail.cuentas and CuentaMail.cuentas[mail] == contrasenia:
+            self.cuenta_mail = mail
+            self.cuenta_iniciada = True
+            print("Sesión iniciada con éxito")
+        else:
+            print("No se pudo iniciar sesión. Verifique los datos ingresados")
     
     def cerrar_sesion(self):
-        pass
+        self.cuenta_mail = None
+        self.cuenta_iniciada = False
+        print("Sesión cerrada con éxito")
     
-    def crear_cuenta(self, cuenta, contrasenia):
+    def crear_cuenta(self, mail, contrasenia):
+        try:
+            cuenta = CuentaMail(mail, contrasenia)
+            print("Cuenta creada con éxito. Inicie sesión para comenzar a utilizarla")
+        except ValueError as e:
+            print(e)
         pass
-    
-    # @classmethod
-    # def validar_cuenta(cls, cuenta):
-    #     pass
-    
-    @staticmethod
-    def validar_contrasenia(contrasenia):
-        pass
-
  
 class CuentaMail():
+    cuentas = {}
+    
     def __init__(self, mail, contrasenia):
         if not self.validar_mail(mail):
             raise ValueError("El mail no es válido")       
         if not self.validar_contrasenia(contrasenia):
             raise ValueError("La contraseña no es válida")
         
-        self.mail = mail
+        self.mail = mail #Debe ser único por eso las validaciones previas
         self.contrasenia = contrasenia
+        self.cuentas[mail] = contrasenia
 
-    @staticmethod
-    def validar_mail(mail):
+    @classmethod
+    def validar_mail(cls, mail):
         # Expresión regular para validar un correo electrónico
         regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        return re.match(regex, mail) is not None #Si la expresión regular coincide con el email, retorna True
+        return (re.match(regex, mail) is not None) and (mail not in cls.cuentas) #Si la expresión regular coincide con el email, retorna True
     
     @staticmethod
     def validar_contrasenia(contrasenia):
@@ -58,7 +70,8 @@ class CuentaMail():
         return re.match(regex, contrasenia) is not None
     
     def __str__(self):
-            return f"La cuenta es: {self.cuenta} y la contrasenia es: {self.contrasenia}"
+            return f"La cuenta es: {self.mail} y la contrasenia es: {self.contrasenia}"
+
 
 class NodoMail():
     def __init__(self, mensaje):
