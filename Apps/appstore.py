c47f0a9d0c0aea1ef910c1723144d242fff64c19
Franco Mutz <fmutz@itba.edu.ar>
2024-10-25 13:27:27 -0300

    DocStrings

--- Apps/appstore.py
+++ Apps/appstore.py
@@ -3,14 +3,55 @@ from Apps.configuracion import Configuracion
 from manejadorCSV import ManejadorCSV
 
 class AppStore(Aplicacion):
+    """
+    Clase que representa la tienda de aplicaciones de un celular
+    
+    Atributos:
+    ----------
+    exportador : ManejadorCSV
+        Manejador para exportar datos a un archivo CSV.
+    aplicaciones_celular : dict
+        Diccionario que contiene las aplicaciones instaladas en el celular.
+    configuracion : Configuracion
+        Configuración del dispositivo, incluyendo almacenamiento disponible.
+        
+    Métodos:
+    --------
+    __init__(self, aplicaciones_celular, configuracion: Configuracion):
+        Inicializa una instancia de AppStore para cada celular, con las aplicaciones instaladas en el celular y la configuración del dispositivo.
+    mostrar_apps_disponibles(self):
+        Muestra las aplicaciones disponibles en la tienda, indicando si ya están instaladas en el celular.
+    descargar_app(self, nombre):
+        Descarga una aplicación de la tienda al celular, si hay suficiente espacio y no está ya instalada.
+    desinstalar_app(self, nombre):
+        Desinstala una aplicación del celular, si no es esencial y está instalada.
+    agregar_descarga(self, nombre):
+        Incrementa el contador de descargas de una aplicación y actualiza el archivo CSV.
+    consultar_tamanio(self, nombre):
+        Consulta el tamaño de una aplicación disponible en la tienda.
+    aplicaciones_disponibles(self):
+        Devuelve una lista de las aplicaciones disponibles en la tienda.
+    __str__(self):
+        Devuelve una representación en cadena de la instancia de AppStore.
+    """
     exportador = ManejadorCSV("appstore.csv")
     
     def __init__(self, aplicaciones_celular, configuracion: Configuracion):
+        """
+        Args:
+            aplicaciones_celular (list): Lista de aplicaciones instaladas en el celular.
+            configuracion (Configuracion): Objeto de configuración que contiene las configuraciones del celular
+        """
         super().__init__("AppStore", 200, True)
         self.aplicaciones_celular = aplicaciones_celular
         self.configuracion = configuracion
     
     def mostrar_apps_disponibles(self):
+        """Muestra las aplicaciones disponibles en la tienda, indicando si ya están instaladas en el celular.
+        
+        Returns:
+            None
+        """
         aplicaciones_disponibles = self.aplicaciones_disponibles()
         for app in aplicaciones_disponibles:
             if app[0] not in self.aplicaciones_celular:
