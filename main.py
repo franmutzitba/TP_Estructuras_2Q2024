6b39a01f3b7333a3f20f9346ea539f86b0d82471
alechof <ahoffmann@itba.edu.ar>
2024-11-01 00:32:27 -0300

    pruebas llamadas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from celular import Celular
 from Apps.mail import *
 from manejadorCSV import ManejadorCSV
+from Apps.telefono import TelefonoApp
 
 if "__main__" == __name__:
     exportadorCelulares = ManejadorCSV("celulares.csv")
@@ -8,13 +9,15 @@ if "__main__" == __name__:
     celular2 = Celular("Samsung de Juan", "Samsung Galaxy S21", "987654321", "Android", "6GB", "128GB")
     celular3 = Celular("Motorola de Pedro", "Motorola G9", "456789123", "Android", "4GB", "32GB")
     celular4 = Celular("Huawei de Ana", "Huawei P40", "654321987", "Android", "8GB", "256GB")
-    celular1.encencer_dispositivo()
+    celular1.encender_dispositivo()
+    celular2.encender_dispositivo()
     # celular2.encencer_dispositivo()
-    try:
+    '''try:
         celular1.menu_navegacion()
     except ValueError as e:
-        print(e)
+        print(e)'''
 
     telefono1 = celular1.aplicaciones["Telefono"]
     telefono2 = celular2.aplicaciones["Telefono"]
-    nro 
+    telefono1.iniciar_llamada("987654321", 5)
+
